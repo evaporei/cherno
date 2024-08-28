341343b12011934843203fe01e892f415bb470cc
Eva Pace <eba.pachi@gmail.com>
2024-08-28 15:29:49 -0300

    glfw window

---
+++ src/main.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+fn processInput(window: *const glfw.Window) void {
+    if (window.getKey(glfw.Key.escape) == glfw.Action.press) {
+        window.setShouldClose(true);
+    }
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    const window = glfw.Window.create(640, 480, "cherno opengl tutorial", null, null, .{
+        .context_version_major = gl.info.version_major,
+        .context_version_minor = gl.info.version_minor,
+        .opengl_profile = .opengl_core_profile,
+        .opengl_forward_compat = true,
+    }) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+    defer glfw.makeContextCurrent(null);
+
+    // enables VSync, to avoid unnecessary drawing
+    glfw.swapInterval(1);
+
+    while (!window.shouldClose()) {
+        processInput(&window);
+
+        window.swapBuffers();
+        glfw.pollEvents();
+    }
+}
