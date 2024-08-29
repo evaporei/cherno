d9dc643fe5543f1a15058464463411eaf701ccd1
Eva Pace <eba.pachi@gmail.com>
2024-08-28 22:12:53 -0300

    i dunno why my Square doesn't work

--- src/main.zig
+++ src/main.zig
@@ -73,6 +73,82 @@ fn glCheckError() void {
     }
 }
 
+const positions: [8]f32 = [_]f32{
+    -0.5, -0.5,
+    0.5,  0.5,
+    0.5,  -0.5,
+    -0.5, 0.5,
+};
+const indices: [6]u32 = [_]u32{
+    0, 1, 2,
+    0, 1, 3,
+};
+
+const Square = struct {
+    const Self = @This();
+    vao: c_uint,
+    buffer: c_uint,
+    ibo: c_uint,
+    color: struct { r: f32, g: f32, b: f32 },
+
+    fn init() Self {
+        var vao: c_uint = undefined;
+        gl.GenVertexArrays(1, (&vao)[0..1]);
+        gl.BindVertexArray(vao);
+
+        var buffer: c_uint = undefined;
+        gl.GenBuffers(1, (&buffer)[0..1]);
+        gl.BindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.BufferData(gl.ARRAY_BUFFER, positions.len * @sizeOf(f32), &positions, gl.STATIC_DRAW);
+        // zig fmt: off
+        gl.VertexAttribPointer(
+            0, // position in shader
+            2, // each has 2
+            gl.FLOAT, // type
+            gl.FALSE, // no normals translation
+            2 * @sizeOf(f32), // each vertex has 2 floats
+            undefined
+        );
+        // zig fmt: on
+        gl.EnableVertexAttribArray(0);
+
+        var ibo: c_uint = undefined;
+        gl.GenBuffers(1, (&ibo)[0..1]);
+        gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+        gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, indices.len * @sizeOf(u32), &indices, gl.STATIC_DRAW);
+        std.debug.print("VAO: {d}, VBO: {d}, IBO: {d}\n", .{ vao, buffer, ibo });
+
+        return Square{ .vao = vao, .buffer = buffer, .ibo = ibo, .color = .{ .r = 1, .g = 1, .b = 1 } };
+    }
+
+    fn setColor(self: *Self, r: f32, g: f32, b: f32) void {
+        self.color = .{ .r = r, .g = g, .b = b };
+    }
+
+    fn draw(self: Self, program: c_uint) void {
+        // std.debug.print("VAO: {d}, VBO: {d}, IBO: {d}\n", .{ self.vao, self.buffer, self.ibo });
+        gl.BindVertexArray(self.vao);
+        gl.BindBuffer(gl.ARRAY_BUFFER, self.buffer);
+        gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ibo);
+        const location = gl.GetUniformLocation(program, "u_Color");
+        std.debug.assert(location != -1);
+        gl.Uniform4f(location, self.color.r, self.color.g, self.color.b, 1.0);
+        gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
+    }
+
+    fn deinit(self: Self) void {
+        var vao: c_uint = self.vao;
+        var buffer: c_uint = self.buffer;
+        var ibo: c_uint = self.ibo;
+        gl.DeleteVertexArrays(1, (&vao)[0..1]);
+        gl.BindVertexArray(0);
+        gl.DeleteBuffers(1, (&buffer)[0..1]);
+        gl.BindBuffer(gl.ARRAY_BUFFER, 0);
+        gl.DeleteBuffers(1, (&ibo)[0..1]);
+        gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
+    }
+};
+
 var gl_procs: gl.ProcTable = undefined;
 
 pub fn main() !void {
@@ -111,55 +187,34 @@ pub fn main() !void {
     gl.makeProcTableCurrent(&gl_procs);
     defer gl.makeProcTableCurrent(null);
 
-    // not in the cherno tutorial, I found out in the comments
-    var vao: c_uint = undefined;
-    gl.GenVertexArrays(1, (&vao)[0..1]);
-    defer gl.DeleteVertexArrays(1, (&vao)[0..1]);
+    var square = Square.init();
+    defer square.deinit();
+    square.setColor(0.8, 0.3, 0.8);
 
-    gl.BindVertexArray(vao);
-    defer gl.BindVertexArray(0);
-
-    const positions = [_]f32{
-        -0.5, -0.5,
-        0.5,  0.5,
-        0.5,  -0.5,
-        -0.5, 0.5,
-    };
-
-    const indices = [_]u32{
-        0, 1, 2,
-        0, 1, 3,
-    };
-
-    var buffer: c_uint = undefined;
-    // create(how many, id)
-    gl.GenBuffers(1, (&buffer)[0..1]);
-    defer gl.DeleteBuffers(1, (&buffer)[0..1]);
-    // select
-    gl.BindBuffer(gl.ARRAY_BUFFER, buffer);
-    defer gl.BindBuffer(gl.ARRAY_BUFFER, 0);
-    // load buffer to GPU
-    gl.BufferData(gl.ARRAY_BUFFER, positions.len * @sizeOf(f32), &positions, gl.STATIC_DRAW);
-
-    // zig fmt: off
-    gl.VertexAttribPointer(
-        0, // position in shader
-        2, // each has 2
-        gl.FLOAT, // type
-        gl.FALSE, // no normals translation
-        2 * @sizeOf(f32), // each vertex has 2 floats
-        undefined
-    );
-    // zig fmt: on
-    // enable attrib position 0 (above)
-    gl.EnableVertexAttribArray(0);
-
-    var ibo: c_uint = undefined;
-    gl.GenBuffers(1, (&ibo)[0..1]);
-    gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, indices.len * @sizeOf(u32), &indices, gl.STATIC_DRAW);
-    defer gl.DeleteBuffers(1, (&ibo)[0..1]);
-    defer gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
+    // var vao: c_uint = undefined;
+    // gl.GenVertexArrays(1, (&vao)[0..1]);
+    // gl.BindVertexArray(vao);
+    //
+    // var buffer: c_uint = undefined;
+    // gl.GenBuffers(1, (&buffer)[0..1]);
+    // gl.BindBuffer(gl.ARRAY_BUFFER, buffer);
+    // gl.BufferData(gl.ARRAY_BUFFER, positions.len * @sizeOf(f32), &positions, gl.STATIC_DRAW);
+    // // zig fmt: off
+    // gl.VertexAttribPointer(
+    //     0, // position in shader
+    //     2, // each has 2
+    //     gl.FLOAT, // type
+    //     gl.FALSE, // no normals translation
+    //     2 * @sizeOf(f32), // each vertex has 2 floats
+    //     undefined
+    // );
+    // // zig fmt: on
+    // gl.EnableVertexAttribArray(0);
+    //
+    // var ibo: c_uint = undefined;
+    // gl.GenBuffers(1, (&ibo)[0..1]);
+    // gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    // gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, indices.len * @sizeOf(u32), &indices, gl.STATIC_DRAW);
 
     const vertexShader = try readFile(allocator, "res/shaders/basic.vertex.shader");
     defer vertexShader.deinit();
@@ -170,14 +225,7 @@ pub fn main() !void {
     const program = createProgram(vertexShader.items, fragmentShader.items);
     gl.UseProgram(program);
 
-    const location = gl.GetUniformLocation(program, "u_Color");
-    std.debug.assert(location != -1);
-    gl.Uniform4f(location, 0.8, 0.3, 0.8, 1.0);
-
     glCheckError();
-
-    var r: f32 = 0;
-    var increment: f32 = 0.05;
 
     while (!window.shouldClose()) {
         processInput(&window);
@@ -185,16 +233,11 @@ pub fn main() !void {
         gl.ClearColor(0.0, 0.0, 0.0, 1.0);
         gl.Clear(gl.COLOR_BUFFER_BIT);
 
-        gl.Uniform4f(location, r, 0.3, 0.8, 1.0);
-        gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
-
-        if (r > 1.0) {
-            increment = -0.05;
-        } else if (r < 0.0) {
-            increment = 0.05;
-        }
-
-        r += increment;
+        square.draw(program);
+        // const location = gl.GetUniformLocation(program, "u_Color");
+        // std.debug.assert(location != -1);
+        // gl.Uniform4f(location, 0.8, 0.3, 0.8, 1.0);
+        // gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
 
         window.swapBuffers();
         glfw.pollEvents();
