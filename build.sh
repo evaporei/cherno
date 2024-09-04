ad20da26f64a1b8ba20cbad090ccbf9111c3f210
Eva Pace <eba.pachi@gmail.com>
2024-09-04 17:52:42 -0300

    opengl working (I'm glaaadd)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-eval cc main.cpp $(pkg-config --libs --cflags glfw3) -o build/cherno
+eval cc main.cpp glad/src/glad.c $(pkg-config --libs --cflags glfw3) -Iglad/include -framework OpenGL -o build/cherno
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 void processInput(GLFWwindow *window)
@@ -12,6 +13,11 @@ int main(void)
 {
     if (!glfwInit())
         return -1;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *window = glfwCreateWindow(640, 480, "cherno opengl", NULL, NULL);
 
@@ -23,9 +29,17 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
+    if (gladLoadGL() == GL_FALSE)
+    {
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0, 0, 0, 1);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
