f6edc08853fa1832f9b9a923f0855b76991fc172
Eva Pace <eba.pachi@gmail.com>
2024-09-04 19:28:05 -0300

    open shader files

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-eval cc main.cpp glad/src/glad.c $(pkg-config --libs --cflags glfw3) -Iglad/include -framework OpenGL -o build/cherno
+eval clang++ main.cpp glad/src/glad.c $(pkg-config --libs --cflags glfw3) -Iglad/include -framework OpenGL -o build/cherno
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
+#include "stdlib.h"
+#include "stdio.h"
 
 void processInput(GLFWwindow *window)
 {
@@ -8,6 +11,41 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, 1);
     }
 }
+
+const char* readFile(const char *filepath)
+{
+    FILE *file = fopen(filepath, "r");
+    if (!file)
+    {
+        std::cerr << "failed to open file: " << filepath << std::endl;
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    rewind(file);
+
+    char *contents = (char*) malloc(fileSize + 1);
+
+    fread(contents, 1, fileSize, file);
+    contents[fileSize] = '\0';
+
+    fclose(file);
+
+    return (const char*) contents;
+}
+
+float positions[] = {
+    -0.5f, -0.5f,
+    0.5, 0.5,
+    0.5, -0.5,
+    -0.5, 0.5
+};
+
+unsigned int indices[] = {
+    0, 1, 2,
+    0, 1, 3
+};
 
 int main(void)
 {
@@ -33,6 +71,32 @@ int main(void)
     {
         return -1;
     }
+
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), &positions, GL_STATIC_DRAW);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+
+    const char *vertexShaderSrc = readFile("res/shaders/basic.vertex.shader");
+
+    std::cout << vertexShaderSrc << std::endl;
+
+    free((void*) vertexShaderSrc);
+
+    const char *fragmentShaderSrc = readFile("res/shaders/basic.fragment.shader");
+
+    std::cout << fragmentShaderSrc << std::endl;
+
+    free((void*) fragmentShaderSrc);
 
     while (!glfwWindowShouldClose(window))
     {
