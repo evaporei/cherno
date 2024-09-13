18594f9d542731c252cd4d58fcd8eb1266360fcc
Eva Pace <eba.pachi@gmail.com>
2024-09-13 14:24:43 -0300

    imgui!

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include "stdio.h"
 
 #include "glad/glad.h"
-#include "GLFW/glfw3.h"
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/ext/matrix_transform.hpp>
@@ -83,19 +86,25 @@ int main(void) {
     glm::mat4 projection = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f);
     // "camera", to move to left, we must move everything to the right
     glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-    // translation, rotation and scale
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-    
-    // order matters because of OpenGL's column based ordering
-    glm::mat4 mvp = projection * view * model;
+
+    glm::vec3 translation(200, 200, 0);
 
     struct Shader shader = shader_init("res/shaders/basic.vertex.shader", "res/shaders/basic.fragment.shader");
 
-    struct Texture texture = texture_init("res/textures/cherno_logo.png");
+    struct Texture _ = texture_init("res/textures/cherno_logo.png");
 
     shader_set_uniform_4f(&shader, "u_Color", 0.8, 0.3, 0.8, 1.0);
     shader_set_uniform_1i(&shader, "u_Texture", 0);
-    shader_set_uniform_mat4f(&shader, "u_MVP", mvp);
+
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    // ImGui_ImplOpenGL3_Init("#version 330 core");
+    ImGui_ImplOpenGL3_Init("#version 150");
 
     checkErrors();
 
@@ -104,11 +113,39 @@ int main(void) {
 
         renderer_clear();
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // translation, rotation and scale
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+        
+        // order matters because of OpenGL's column based ordering
+        glm::mat4 mvp = projection * view * model;
+        shader_set_uniform_mat4f(&shader, "u_MVP", mvp);
+
         renderer_draw(&shader, &vao, &ibo);
+
+        {
+            ImGui::SliderFloat3("translation", &translation.x, 0.0f, 960.0f);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
